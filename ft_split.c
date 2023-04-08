16028768396a4224fbf2e299707de2a7559e7fd1
smelicha <smelicha@student.42heilbronn.de>
2023-04-08 23:42:08 +0200

    split update

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 16:07:56 by smelicha          #+#    #+#             */
-/*   Updated: 2023/04/08 19:25:47 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/04/08 23:36:09 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,7 @@ int main()
 	i = 0;
 	res = ft_split(test, ',');
 	printf("\n\n\nMAINMAINMAINMAINMAINMAINMAINMAINMAIN\n\n\n");
+	printf("string to split: %s\n\n", test);
 	while (*res[i] != '\0')
 	{
 		printf("from res: |%s|\n", res[i]);
@@ -93,10 +94,83 @@ int main()
 	return (0);
 }
 
+static size_t	wordc(const char *s, char c)
+{
+	int		wflag;
+	size_t	wc;
 
+	wc = 0;
+	wflag = 0;
+	while (*s)
+	{
+		if (*s == c)
+			wflag = 0;
+		if (wflag == 0)
+		{
+			wflag = 1;
+			wc++;
+		}
+		s++;
+	}
+	return (wc);
+}
 
+static size_t	wordlen(const char *s, char c)
+{
+	size_t	l;
 
-int		wordl(char *s, char c)
+	l = 0;
+	while(*s && *s != c)
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
+static void		dumpitintoit(char **ret, const char *s, char c)
+{
+	size_t	i;
+	size_t	l;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s == c)
+			s++;
+		else
+		{
+			l = wordlen(s, c);
+			ret[i] = malloc((l + 1) * (sizeof(char)));
+			if (ret[i] == NULL)
+			{
+				while (i-- > 0)
+					free(ret[i]);
+				free(ret);
+			}
+			ft_memcpy(ret[i], (char *)s, l);
+			ret[i][l + 1] = '\0';
+			s += l;
+			i++;
+		}
+	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	wc;
+	char	**ret;
+
+	wc = wordc(s, c);
+	ret = malloc((wc + 1) * (sizeof(char *)));
+	if (!ret)
+		return (NULL);
+	dumpitintoit(ret, s, c);
+	ret[wc + 1] = NULL;
+	return (ret);
+}
+
+/*int		wordl(char *s, char c)
 {
 	int	i;
 	
@@ -192,3 +266,4 @@ char	**ft_split(char const *s, char c)
 //	free(temp);
 	return (ret);
 }
+*/
