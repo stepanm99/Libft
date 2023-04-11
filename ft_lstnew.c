752b48e8f60532eea8c7fe93e700aa3c25a9cb8f
smelicha <smelicha@student.42heilbronn.de>
2023-04-11 20:58:10 +0200

    test

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 18:43:32 by smelicha          #+#    #+#             */
-/*   Updated: 2023/04/11 20:40:41 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/04/11 20:57:58 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_list	*ft_lstnew(void *content)
 {
 	int	a;
-
+	trt;
 	a = 5;
 	return (&a);
 }
