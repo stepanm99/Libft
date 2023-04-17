55362ef72b64f357d7849bdd77cf0130fa484e0b
smelicha <smelicha@student.42heilbronn.de>
2023-04-17 22:49:12 +0200

    everything works now

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:09:49 by smelicha          #+#    #+#             */
-/*   Updated: 2023/04/17 20:49:03 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/04/17 21:50:23 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		if (!new_link)
 		{
 			ft_lstclear(&new_list, del);
-			ft_lstclear(&lst, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_link);
@@ -41,21 +40,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_list);
 }
-
-
-/*	t_list	*new;
-	t_list	*temp1;
-	t_list	*temp2;
-
-	if (!lst || !f)
-		return (NULL);
-	new = ft_lstnew((f)(lst->content));
-	temp1 = lst->next;
-	temp2 = new;
-	while (temp1)
-	{
-		temp2->next = ft_lstnew(temp1->content);
-		temp1 = temp1->next;
-		temp2 = temp2->next;
-	}
-	return (new);*/
