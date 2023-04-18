52647368420eab5f894d045d384e1ac255edc309
smelicha <smelicha@student.42heilbronn.de>
2023-04-18 22:33:49 +0200

    cosmetic changes

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:09:49 by smelicha          #+#    #+#             */
-/*   Updated: 2023/04/17 21:50:23 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/04/18 22:30:02 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst || !f)
 		return (NULL);
 	new_link = (ft_lstnew((*f)(lst->content)));
-	if (!new_link)
+	if (new_link == NULL)
 	{
-		ft_lstclear(&lst, del);
-		return (NULL);
+		(del)(new_link->content);
+		return (ft_lstclear(&lst, del), NULL);
 	}
 	new_list = new_link;
 	lst = lst->next;
 	while (lst)
 	{
 		new_link = ft_lstnew((*f)(lst->content));
-		if (!new_link)
+		if (new_link == NULL)
 		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
+			(del)(new_link->content);
+			return (ft_lstclear(&new_list, del), NULL);
 		}
 		ft_lstadd_back(&new_list, new_link);
 		lst = lst->next;
